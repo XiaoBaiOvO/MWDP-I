02966dd5bfa778f7e63ec6f8e3b7f535fe26ce2c
hmq <gl34daisy@163.com>
2022-01-27 01:30:48 +0800

    update

---
+++ src/main/java/com/mwdp/MwdpIApplication.java
@@ -0,0 +1,13 @@
+package com.mwdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwdpIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwdpIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mwdp/MwdpIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwdpIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
